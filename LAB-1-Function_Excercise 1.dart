03fb294b6cf1e26e6d3ecb8ce697c0f6967a7d87
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-08 03:47:20 -1000

    Create LAB-1-Function_Excercise 1.dart

---
+++ LAB-1-Function_Excercise 1.dart
@@ -0,0 +1,12 @@
+void main() {
+  // I first Called the function to calculate the sum of two numbers
+  double result = Adder(3, 4);
+
+  // Print the result
+  print('The sum of the two numbers is: $result');
+}
+
+// Function to calculate the sum of two numbers
+double Adder(double num1, double num2) {
+  return num1 + num2;
+}
