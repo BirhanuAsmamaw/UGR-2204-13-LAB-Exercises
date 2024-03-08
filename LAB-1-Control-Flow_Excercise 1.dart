b1d79163cec4d62c58b7d373212c994b34e84d16
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-08 03:24:36 -1000

    Create LAB-1-Control-Flow_Excercise 1.dart

---
+++ LAB-1-Control-Flow_Excercise 1.dart
@@ -0,0 +1,10 @@
+void main() {
+  int number = 15; // the number is random it can be changed
+  
+  // Here is how to check if the number is even or odd using the modulus operator
+  if (number % 2 == 0) {
+    print('$number is even.');
+  } else {
+    print('$number is odd.');
+  }
+}
